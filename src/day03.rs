d94ef70642fc4a286a4ca3961f3dcc76ecec9974
Sam Desborough <sam.desborough@gmail.com>
2023-12-06 22:04:35 +0000

    day 3, part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 const LINE_LENGTH: usize = 140;
 
 #[aoc(day3, part1)]
@@ -20,7 +22,10 @@ pub fn solve1(input: &str) -> i32 {
         } else {
             current_number.push(c);
 
-            if adjacent_to_symbol(&chars, i, LINE_LENGTH) {
+            if neighbours(&chars, i, LINE_LENGTH)
+                .iter()
+                .any(|&(_, c)| is_symbol(c))
+            {
                 is_part_number = true;
             }
         }
@@ -29,30 +34,82 @@ pub fn solve1(input: &str) -> i32 {
     sum
 }
 
-fn adjacent_to_symbol(
+#[aoc(day3, part2)]
+pub fn solve2(input: &str) -> i32 {
+    let mut sum = 0;
+
+    let chars = input.chars().enumerate().collect::<Vec<_>>();
+
+    for &(i, c) in &chars {
+        if c == '*' {
+            let ns = neighbours(&chars, i, LINE_LENGTH);
+
+            let numeric_neighbour_cells = ns
+                .iter()
+                .filter(|(_, c)| c.is_digit(10))
+                .collect::<Vec<_>>();
+
+            let mut nums = HashSet::new();
+
+            for &(num_idx, _) in numeric_neighbour_cells {
+                let num = read_number_at(&chars, num_idx).unwrap();
+                nums.insert(num);
+            }
+
+            if nums.len() == 2 {
+                sum += nums.iter().product::<i32>();
+            }
+        }
+    }
+
+    sum
+}
+
+fn read_number_at(indexed_chars: &[(usize, char)], num_idx: usize) -> Option<i32> {
+    let mut num_str = String::new();
+    let mut i = num_idx;
+
+    loop {
+        if i == 0 || !indexed_chars[i - 1].1.is_digit(10) {
+            break;
+        }
+        i -= 1;
+    }
+
+    loop {
+        if !indexed_chars[i].1.is_digit(10) {
+            break;
+        }
+        num_str.push(indexed_chars[i].1);
+        i += 1
+    }
+
+    num_str.parse().ok()
+}
+
+fn is_symbol(c: char) -> bool {
+    c != '.' && !c.is_digit(10) && c != '\n'
+}
+
+fn neighbours(
     indexed_chars: &[(usize, char)],
     index_to_check: usize,
     line_length: usize,
-) -> bool {
-    fn is_symbol(c: char) -> bool {
-        c != '.' && !c.is_digit(10) && c != '\n'
-    }
+) -> Vec<(usize, char)> {
+    let mut result = vec![];
 
     for y in 0i32..3 {
         for x in 0i32..3 {
             let idx = (index_to_check as i32 + x - 1) + ((y - 1) * (line_length + 1) as i32);
 
-            if indexed_chars
+            indexed_chars
                 .get(clamp(0, indexed_chars.len() as i32, idx) as usize)
-                .map(|&(_, c)| is_symbol(c))
-                .unwrap_or(false)
-            {
-                return true;
-            }
+                .into_iter()
+                .for_each(|&(i, c)| result.push((i, c)));
         }
     }
 
-    false
+    result
 }
 
 fn clamp<T: PartialOrd>(min: T, max: T, x: T) -> T {
